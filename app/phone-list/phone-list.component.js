ddd85e7cee31ab5a07f913ff4bbcd353c34dd8b6
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-05 15:21:33 +0300

    file organization

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,13 +1,11 @@
-angular.module('phonecatApp').component('phoneList', {
-    template: 
-
-        '<ul>' +
-          '<li ng-repeat="phone in $ctrl.phones">' +
-            '<span>{{phone.name}}</span>' +
-            '<p>{{phone.snippet}}</p>' +
-          '</li>' +
-        '</ul>',
-
+angular.module('phoneList').component('phoneList', {
+  template:
+  '<ul>' +
+    '<li ng-repeat="phone in $ctrl.phones">' +
+      '<span>{{phone.name}}</span>' +
+      '<p>{{phone.snippet}}</p>' +
+    '</li>' +
+  '</ul>',      
         controller: function PhoneListController() {
             this.phones = [
               {
